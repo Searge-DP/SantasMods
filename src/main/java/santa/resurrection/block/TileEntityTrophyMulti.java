4e3765fe94184d2aaeec85b554a1588276fb6492
satanicsanta <elifosterwy@gmail.com>
2014-10-25 23:33:19 -0700

    Almost done, just need to figure out how to make it check for redstone.

--- src/main/java/santa/resurrection/block/TileEntityTrophyMulti.java
+++ src/main/java/santa/resurrection/block/TileEntityTrophyMulti.java
@@ -1,11 +1,11 @@
 package santa.resurrection.block;
 
 import net.minecraft.entity.item.EntityFireworkRocket;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
-
-import java.util.HashMap;
-import java.util.Map;
+import net.minecraft.world.World;
+import santa.resurrection.LogHelper;
 
 /**
  * Created by elijahfoster-wysocki on 10/25/14.
@@ -14,11 +14,33 @@ public class TileEntityTrophyMulti extends TileEntity {
 
 
     private int x, y, z;
+    private double motionX, motionY, motionZ;
+    private boolean wiresProvidePower;
+
 
     @Override
     public  void updateEntity(){
         super.updateEntity();
+    }
 
+    public void onChange(EntityFireworkRocket rocket, EntityPlayer player, World world){
+
+        /*
+        ItemStack itemstack = EntityFireworkRocket.dataWatcher.getWatchableObjectItemStack(8);
+        NBTTagCompound nbttag = null;
+
+        nbttag = itemstack.getTagCompound().getCompoundTag("Fireworks");
+        */
+
+        rocket.posX = player.posX;
+        rocket.posY = player.posY + 1.0D;
+        rocket.posZ = player.posZ;
+
+        if(world.isBlockIndirectlyGettingPowered(x, y, z)){
+            //this.worldObj.makeFireworks(this.x, this.y, this.z, this.motionX, this.motionY, this.motionZ, nbttag);
+            world.spawnEntityInWorld(rocket);
+            LogHelper.info("TE activated");
+        }
     }
 
     public void reset(){
